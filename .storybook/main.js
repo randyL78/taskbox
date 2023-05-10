392286accfa8d08a86e014ae3207f8a76589124f
Randy Layne <randydavidl78@gmail.com>
2023-05-10 19:16:52 -0400

    Add accessibility storybook package and fix violations

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,19 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.js'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y"
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  docs: {
+    autodocs: 'tag'
   },
   features: {
     interactionsDebugger: true,
